6c310853dd87746a1f24220f0280076531b6a8c4
Sylwia <s.brodacka@gmail.com>
2017-09-07 17:30:12 +0200

    stack build called via bash

---
+++ build_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+stack build --stack-yaml=build/backend/stack.yaml --verbose --no-terminal --only-dependencies --install-ghc --test -j2 --ghc-options=-j2 --ghc-options=-O2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
+stack build --stack-yaml=luna-studio/stack.yaml --no-terminal --only-dependencies --install-ghc --fast -j2 --ghc-options=-j2 --ghc-options=-O2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
+stack build --stack-yaml=runner/stack.yaml --only-dependencies --no-terminal --install-ghc --fast -j2 --ghc-options=-j2 --ghc-options=-O2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
