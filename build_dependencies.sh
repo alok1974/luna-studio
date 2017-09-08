2521ab43f4ac360116b7b0f4a3db41094b79394f
Sylwia <s.brodacka@gmail.com>
2017-09-08 11:43:42 +0200

    build frontend with timeout

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-stack build --stack-yaml=luna-studio/stack.yaml --no-terminal --only-dependencies --install-ghc -j2 --ghc-options=-j2 --ghc-options=-O2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
+
 stack build --stack-yaml=runner/stack.yaml --only-dependencies --no-terminal --install-ghc -j2 --ghc-options=-j2 --ghc-options=-O2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
 
 
-stack build --stack-yaml luna-studio/stack.yaml --no-terminal -j2 --ghc-options=-j2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
+
 stack build --stack-yaml runner/stack.yaml --no-terminal --copy-bins -j2 --ghc-options=-j2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
