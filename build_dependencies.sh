8753efa4e65aafe69f3b0e5f3f46f18854293d5e
Sylwia <s.brodacka@gmail.com>
2017-09-11 15:08:48 +0200

    test cache

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -2,7 +2,7 @@
 
 
 stack build --stack-yaml=runner/stack.yaml --only-dependencies --no-terminal --install-ghc -j2 --ghc-options=-j2 --ghc-options=-O2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
-
+ 
 
 
 stack build --stack-yaml runner/stack.yaml --no-terminal --copy-bins -j2 --ghc-options=-j2 --ghc-options="+RTS -M3G -RTS" +RTS -N1 -RTS
